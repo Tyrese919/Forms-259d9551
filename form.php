8db5fb7e3e62eb9111818f06d0074ab8344778d9
Tyrese919 <t.vanschagen@roc-dev.com>
2020-01-08 11:00:08 +0100

    Uitwerking van de exercise

--- form.php
+++ form.php
@@ -5,8 +5,12 @@
   </head>
   <body>
 
-      <input type="text" value="Voer je E-mailaderes in" size="17">
+    <form class="" action="index.html" method="post">
+
+      <input type="text" placeholder="Voer je E-mailaderes in" size="17">
       <input type="submit" value="Verstuur">
+      
+    </form>
 
   </body>
 </html>
